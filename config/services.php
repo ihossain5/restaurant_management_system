276660b8481da4c8e097ce879520a235b25bae87
ihossain5 <ismail.hossain5169@gmail.com>
2021-11-25 17:50:02 +0600

    google credentials modified

--- config/services.php
+++ config/services.php
@@ -32,13 +32,18 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/login/google/callback',
+        'redirect' => env('GOOGLE_CLIENT_REDIRECT_URL'),
+    ],
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => env('FACEBOOK_CLIENT_REDIRECT_URL'),
     ],
 
-    'facebook' => [
-        'client_id'     => '1184181885409366',
-        'client_secret' => 'c7da528059bab23f5e3b6ff5d33b6b24',
-        'redirect'      => 'http://127.0.0.1:8000/auth/facebook/callback',
-    ],
+    // 'facebook' => [
+    //     'client_id'     => '1184181885409366',
+    //     'client_secret' => 'c7da528059bab23f5e3b6ff5d33b6b24',
+    //     'redirect'      => 'http://127.0.0.1:8000/auth/facebook/callback',
+    // ],
 
 ];
